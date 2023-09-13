c73fe24908bcf5f3c327e315ab1b4273d669bed8
Dreamer2345 <thomaseab@gmail.com>
2023-09-13 14:09:56 +0100

    Update Documentation

---
+++ extras/docs/html/search/all_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['library_0',['TCode Library',['../md_tcode__library_2_r_e_a_d_m_e.html',1,'']]]
+];
---
+++ extras/docs/html/search/all_b.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['saving_20settings_20with_20spiffs_0',['an interface for saving settings with SPIFFS',['../class_settings.html#provides',1,'']]],
+  ['set_1',['set',['../class_t_code_axis.html#abeb2b724b394b3e58cad2560f40b6fbe',1,'TCodeAxis']]],
+  ['setsetting_2',['setSetting',['../class_settings.html#a53aef8e6af31980e336d53717df54bde',1,'Settings']]],
+  ['settings_3',['settings',['../class_settings.html#is',1,'able to be overwritten by child classes inhereting from Settings'],['../class_settings.html',1,'Settings']]],
+  ['settings_20with_20spiffs_4',['an interface for saving settings with SPIFFS',['../class_settings.html#provides',1,'']]],
+  ['settingsusage_5',['SettingsUsage',['../struct_settings_usage.html',1,'']]],
+  ['spiffs_6',['an interface for saving settings with SPIFFS',['../class_settings.html#provides',1,'']]],
+  ['stop_7',['stop',['../class_t_code_axis.html#a8ee7bd84b2b1b1732d3d54ca90672abe',1,'TCodeAxis::stop()'],['../class_t_code.html#ae735e3cecc36adf7ac216aa4db29d0f7',1,'TCode::stop()']]]
+];
