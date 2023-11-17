5810c78569a966d44868e97cb3db73eab59a90dc
Dreamer2345 <thomaseab@gmail.com>
2023-11-17 09:57:51 +0000

    Update TCode.h

--- src/TCode.h
+++ src/TCode.h
@@ -211,7 +211,7 @@ private:
     TCodeAxis *getAxisFromName(const char *name);
     TCodeAxis *getAxisFromID(const TCode_ChannelID &id);
     void executeNextBufferCommand();
-    size_t getNextCommand(unsigned char *buffer, size_t &buffer_length);
+    size_t getNextCommand(unsigned char *buffer, size_t buffer_length);
     void readCommand(unsigned char *buffer, size_t length);
 
     void runAxisCommand(TCode_Axis_Command &command);
